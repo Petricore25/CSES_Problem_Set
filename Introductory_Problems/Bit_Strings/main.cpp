8011ce297852deb8279ab3223501fcf43631c198
Petricore25 <petricore25@gmail.com>
2025-08-23 12:51:50 +0200

    Bit Strings

--- Introductory_Problems/Bit_Strings/main.cpp
+++ Introductory_Problems/Bit_Strings/main.cpp
@@ -8,6 +8,7 @@ long long fast_exp(long long base, long long exp)
 {
     long long ans=1LL;
     base%=mod;
+    if(base<0) base+=mod;
     while(exp>0)
     {
         if(exp&1)
@@ -15,11 +16,8 @@ long long fast_exp(long long base, long long exp)
             ans=(ans*base)%mod;
             exp--;
         }
-        else
-        {
-            base=(base*base)%mod;
-            exp>>=1;
-        }
+        base=(base*base)%mod;
+        exp>>=1;
     }
     return ans;
 }
