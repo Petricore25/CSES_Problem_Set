b7813230cd09f4dbd79a54e59ce95f329d91ae65
Petricore25 <petricore25@gmail.com>
2025-08-23 12:48:49 +0200

    Bit Strings

---
+++ Introductory_Problems/Bit_Strings/main.cpp
@@ -0,0 +1,34 @@
+//https://cses.fi/problemset/task/1617
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int mod=1e9+7;
+
+long long fast_exp(long long base, long long exp)
+{
+    long long ans=1LL;
+    base%=mod;
+    while(exp>0)
+    {
+        if(exp&1)
+        {
+            ans=(ans*base)%mod;
+            exp--;
+        }
+        else
+        {
+            base=(base*base)%mod;
+            exp>>=1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    long long n;
+    cin>>n;
+    cout<<fast_exp(2, n);
+    return 0;
+}
